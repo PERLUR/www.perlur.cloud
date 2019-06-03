5201c5ef609f856c30e0a2e3fd21ee1c0a3bacf7
Mark Stopka <mark.stopka@perlur.cloud>
2019-06-03 07:27:29 +0200

    Update WordPress template.

--- src/wordpress/wp-content/themes/www_perlur_cloud/webpack.config.js
+++ src/wordpress/wp-content/themes/www_perlur_cloud/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './assets/js/index.js',
@@ -8,26 +9,44 @@ module.exports = {
     filename: 'js/bundle.js'
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].bundle.css'
-    }),
+    new MiniCssExtractPlugin(
+      {
+        filename: 'css/[name].bundle.css'
+      }
+    ),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: './assets/images',
+          to: 'images/',
+          test: /^(apple-touch-icon|favicon|mstile)-*\.png$/,
+        },
+        {
+          from: './assets/images',
+          to: 'images/',
+          test: /^favicon\.ico$/,
+        },
+      ]
+    )
   ],
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
           MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              // options...
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                // options...
+              }
             }
-          }
         ]
-    }]
+      },
+    ]
   },
 };

c51ad4c63c7bdaef8dea5d619d7d3f6acbfff8c5
Mark Stopka <mark.stopka@perlur.cloud>
2019-06-11 21:02:08 +0200

    Update page title

--- src/wordpress/wp-content/themes/www_perlur_cloud/page.php
+++ src/wordpress/wp-content/themes/www_perlur_cloud/page.php
@@ -7,7 +7,7 @@
 				while (have_posts()) {
 					the_post();
 					// Include page title.
-					the_title( '<h3>', '</h3>' );
+					the_title( '<h2 class="title is-2">', '</h2>' );
 					// Include the page content.
 					the_content();
 				} // End the loop.
